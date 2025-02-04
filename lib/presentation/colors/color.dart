775360d1dd64ecdd1333bd669c2672ba846e0a5d
Your Name <you@example.com>
2025-02-04 15:22:51 +0500

    first coomit

---
+++ lib/presentation/colors/color.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class Mycolors{
+  static const Color whiteColor = Colors.white;
+  static const Color grayColor =  Color(0xFF707B81);
+  static const Color blackgreyColor =  Color(0xFF2B2B2B);
+  static const Color backColor =  Color(0xFFF7F7F9);
+  static const Color textColor =  Color(0xFF6A6A6A);
+  static const Color blueColor =  Color(0xFF48B2E7);
+  static const Color blackColor = Colors.black;
+
+}
+//#707B81
